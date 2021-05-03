2a0d468ea59ab7fa20bc29c87b44a3413766a561
DPontes <dpontes@protonmail.com>
2021-05-03 22:06:16 +0200

    Use of interface Body to allow the use of function Read to correctly read the html body

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func main() {
     os.Exit(1)
   }
 
-  fmt.Println(resp)
+  // an alterative way of creating an empty byte slice with size for 99999 elements
+  // Given that the function Read cannot resize the slice, we need to preemptively alocate enough space for the data that it will handle
+  bs := make([]byte, 99999)
+
+  resp.Body.Read(bs)
+  fmt.Println(string(bs))
 }
